134a7bcced4958f9e863c1b02b45f9f1a5b69e4a
eitai213 <eitaibenami234@gmail.com>
2024-05-24 15:43:18 +0300

    marked an old file

--- 3D_game/client.py
+++ 3D_game/client.py
@@ -55,7 +55,7 @@ class Client:
 
             app.run()
         else:
-            print("Failed to discover server.")
+            print("Failed to discover server...")
 
 
 if __name__ == "__main__":
